2b0f549b8b6974d892e6119b1c5b3c5c6a87133b
vinayak1-coder <82221546+vinayak1-coder@users.noreply.github.com>
2022-02-18 23:51:01 +0530

    reverse the queue

---
+++ src/reverse_of_a_queue.java
@@ -0,0 +1,50 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class reverse_of_a_queue {
+
+    static Queue<Integer> queue;
+    static void Print()
+    {
+        while (!queue.isEmpty()) {
+            System.out.print( queue.peek() + ", ");
+            queue.remove();
+        }
+    }
+
+    static void reversequeue()
+    {
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()) {
+            stack.add(queue.peek());
+            queue.remove();
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.peek());
+            stack.pop();
+        }
+    }
+    public static void main(String args[])
+    {
+        reverse_of_a_queue o = new reverse_of_a_queue();
+        queue = new LinkedList<Integer>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        queue.add(60);
+        queue.add(70);
+        queue.add(80);
+        queue.add(90);
+        queue.add(100);
+
+        reversequeue();
+        System.out.println("reverse");
+        o.Print();
+    }
+}
+
+
